a8d31281eed92d93a6065d5d4f9051cdc7f90863
Akos Toth <ti1lolka@gmail.com>
2020-10-18 17:11:30 +0200

    Create and Update for Activity entity

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,39 +1,33 @@
-import React, {Component} from 'react';
-import './App.css';
-import axios from 'axios';
-import { Header, Icon, List } from 'semantic-ui-react'
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
-class App extends Component{
-  
+class App extends Component {
   state = {
-    values:[]
+    values: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      this.setState({
+        values: response.data,
+      });
+    });
   }
-  
-  componentDidMount(){
-    axios.get('http://localhost:5000/api/values').then(
-      (response) => {
- 
-        this.setState({
-          values: response.data
-        })
-      }
-    )
-  }
-  
-  render(){
+
+  render() {
     return (
       <div>
-        <Header as='h2'>
-    <Icon name='plug' />
-    <Header.Content>Uptime Guarantee</Header.Content>
-  </Header>
-  <List>
-   
-    {this.state.values.map((value: any) => (
-        
-              <List.Item key={value.id}>{value.name}</List.Item>
-            ))}
-  </List>
+        <Header as="h2">
+          <Icon name="plug" />
+          <Header.Content>Uptime Guarantee</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
