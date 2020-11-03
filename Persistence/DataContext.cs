785e0221cbd4d3f9d4fddd0b97cef2d5a18a5e5e
Akos Toth <ti1lolka@gmail.com>
2020-11-03 10:45:47 +0100

    Relation between user andd activity

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -2,26 +2,53 @@ using Domain;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence
-{
-    public class DataContext : IdentityDbContext<AppUser>
-    {
-        public DataContext(DbContextOptions options):base(options)
-        {
+namespace Persistence {
+    public class DataContext : IdentityDbContext<AppUser> {
+        public DataContext (DbContextOptions options) : base (options) { }
+        public DbSet<Value> Values {
+            get;
+            set;
         }
-        public DbSet<Value> Values { get; set; }
-        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Activity> Activities {
+            get;
+            set;
+        }
+        public DbSet<UserActivity> UserActivities {
+            get;
+            set;
+        }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            
-            builder.Entity<Value>()
-                   .HasData(
-                       new Value{Id = 1, Name = "value one"},
-                       new Value{Id = 2, Name = "value two"},
-                       new Value{Id = 3, Name = "value three"}
-                   );
+        protected override void OnModelCreating (ModelBuilder builder) {
+            base.OnModelCreating (builder);
+
+            builder.Entity<Value> ()
+                .HasData (new Value {
+                        Id = 1,
+                            Name = "value one"
+                    },
+                    new Value {
+                        Id = 2,
+                            Name = "value two"
+                    },
+                    new Value {
+                        Id = 3,
+                            Name = "value three"
+                    }
+                );
+            builder.Entity<UserActivity> (x => x.HasKey (ua => new {
+                ua.AppUserId,
+                    ua.ActivityId
+            }));
+
+            builder.Entity<UserActivity> ()
+                .HasOne (a => a.AppUser)
+                .WithMany (a => a.UserActivities)
+                .HasForeignKey (u => u.AppUserId);
+
+            builder.Entity<UserActivity> ()
+                .HasOne (a => a.Activity)
+                .WithMany (u => u.UserActivities)
+                .HasForeignKey (a => a.ActivityId);
         }
     }
 }
