b9e8242e6ac486dbedc9b5926fe653ea386b2a0c
Akos Toth <ti1lolka@gmail.com>
2020-11-14 16:42:34 +0100

    Show followings statistics

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -3,6 +3,9 @@ export interface IProfile {
   userName: string;
   image: string;
   bio: string;
+  following: boolean;
+  followersCount: number;
+  followingsCount: number;
   photos: IPhoto[];
 }
 
