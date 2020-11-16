83a15005f13b01b33530bd40cc2151fa425f8911
Akos Toth <ti1lolka@gmail.com>
2020-11-16 22:19:47 +0100

    env + remove sleep from api calls/ placeholder

--- client-app/src/features/Activities/dashboard/ListItemPlaceholder.tsx
+++ client-app/src/features/Activities/dashboard/ListItemPlaceholder.tsx
@@ -29,6 +29,31 @@ const ActivityListItemPlaceholder = () => {
           </Segment>
         </Segment.Group>
       </Placeholder>
+      <Placeholder fluid style={{ marginTop: 50 }}>
+        <Segment.Group>
+          <Segment style={{ minHeight: 110 }}>
+            <Placeholder>
+              <Placeholder.Header image>
+                <Placeholder.Line />
+                <Placeholder.Line />
+              </Placeholder.Header>
+              <Placeholder.Paragraph>
+                <Placeholder.Line />
+              </Placeholder.Paragraph>
+            </Placeholder>
+          </Segment>
+          <Segment>
+            <Placeholder>
+              <Placeholder.Line />
+              <Placeholder.Line />
+            </Placeholder>
+          </Segment>
+          <Segment secondary style={{ minHeight: 70 }} />
+          <Segment clearing>
+            <Button disabled color='blue' floated='right' content='View' />
+          </Segment>
+        </Segment.Group>
+      </Placeholder>
     </Fragment>
   );
 };
